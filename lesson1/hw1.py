00df5cebb52a32964f21c204fe89326cdc97d6b0
Katerina <furazkinakaterina@gmail.com>
2022-05-10 21:03:05 +0300

    first commit

---
+++ lesson1/hw1.py
@@ -0,0 +1,12 @@
+value = 1 + 5
+print(value)
+15 == 5 + 10
+print(15)
+password = input("Введите пароль->")
+
+original_password = "correct"
+
+if original_password == password:
+    print("Верно")
+else:
+    print("Не верно")
